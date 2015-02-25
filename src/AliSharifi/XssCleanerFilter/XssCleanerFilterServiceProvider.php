32f066fe00a76da51c2ee18f7b3e46497af63db3
alisharifi01 <alisharifi01@gmail.com>
2015-02-25 15:54:42 +0330

    t

---
+++ src/AliSharifi/XssCleanerFilter/XssCleanerFilterServiceProvider.php
@@ -0,0 +1,46 @@
+<?php namespace AliSharifi\XssCleanerFilter;
+
+use Illuminate\Support\ServiceProvider;
+
+class XssCleanerFilterServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('ali-sharifi/xss-cleaner-filter');
+		include __DIR__.'/XSS.php';
+		include __DIR__.'/filters.php';
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
